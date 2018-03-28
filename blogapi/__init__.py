61b82e5591dd1def78104d6bae1bdd1416e6906a
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-03-28 08:52:54 +0000

    Modified blogapi init code and Post column names and added mysql-connector as a requirement

--- blogapi/__init__.py
+++ blogapi/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 import itertools
 
-from blogapi.config import DevelopmentConfig
+from blogapi.config import ProductionConfig as Config 
 from blogapi.models import db
 from blogapi.routes import init_api_routes
 
 # Initialize Flask app and database
 app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
+app.config.from_object(Config)
 db.init_app(app)
 init_api_routes(app)
 
